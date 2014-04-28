952a9e68d10ca210a6eb0db83cfa6e37054018ab
Lukas <lukas@brown.edu>
2014-04-27 21:12:42 -0400

    Footnote Page Test 18

--- script/book.js
+++ script/book.js
@@ -72,10 +72,9 @@ function trimRegions(flowName) {
 // Create Footnotes of hyperlink URLs
 $(document).ready(function(){
   var num_links = $("a").length; // to be the number of footnotes
-  alert(num_links);
   for(i=0; i<num_links; i++){
     var url = $("a").eq(i).attr("href");
-    var pg_num = $("a").eq(i).parents(".page-outer").attr("class"); //.parent().data("page");
+    var pg_num = $("a").eq(i).parents(".page-outer").css("background-color","red"); //.parent().data("page");
     alert(pg_num);
     //$("footnotes").eq(pg_num-1).append("["+i+"] " + url + " <br />");
   }
