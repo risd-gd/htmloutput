a1fbce72dc3122329b007e63207ba762c8cb7ce5
Lukas <lukas@brown.edu>
2014-05-12 11:13:36 -0400

    Scrolling on TOC Works.

--- script/toc.js
+++ script/toc.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 
-  $(".hello").click(function() {
-    $('html, body').animate({
-      scrollTop: $("#about").offset().top
-    }, 800);
+  // strip urls of their / / marks
+  for(var i = 0; i < $(".nav_link").length; i++){
+    var temp = $(".nav_link").eq(i).attr("name");
+    //alert(temp);
+    var end = temp.length - 1;
+    temp = temp.substring(1,end);
+    $(".nav_link").eq(i).attr("name",temp);
+    $(".pagey").eq(i).attr("id",temp);
+  }
 
+  var scrollTop = $(document).scrollTop();
+
+  $(".nav_link").click(function() {
+    var scroll_to = $(this).attr("name");
+    $('html, body').animate({
+      scrollTop: $("#"+scroll_to).offset().top + 320
+    }, 800);
   });
 
 });
